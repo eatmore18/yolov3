c3137d0418a2bbd1b74c186280dac918c0742b4b
eatmore18 <reza_alidoost@hotmail.com>
2022-07-17 05:40:56 +0000

    Add the convolutional and upsampling block

--- detector/darknet.py
+++ detector/darknet.py
@@ -36,5 +36,55 @@ def parse_cfg(cfgfile):
     blocks.append(block)
   
   return blocks
-  
-        
+
+def create_modules(blocks):
+  net_info = blocks[0]     #Captures the information about the input and pre-processing    
+  module_list = nn.ModuleList()
+  prev_filters = 3
+  output_filters = []
+  for index, x in enumerate(blocks[1:]):
+    module = nn.Sequential()
+    if (x["type"] == "convolutional"):
+      #Get the info about the layer
+      activation = x["activation"]
+      try:
+        batch_normalize = int(x["batch_normalize"])
+        bias = False
+      except:
+        batch_normalize = 0
+        bias = True
+
+      filters= int(x["filters"])
+      padding = int(x["pad"])
+      kernel_size = int(x["size"])
+      stride = int(x["stride"])
+
+      if padding:
+        pad = (kernel_size - 1) // 2
+      else:
+        pad = 0
+
+      #Add the convolutional layer
+      conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias = bias)
+      module.add_module("conv_{0}".format(index), conv)
+
+      #Add the Batch Norm Layer
+      if batch_normalize:
+        bn = nn.BatchNorm2d(filters)
+        module.add_module("batch_norm_{0}".format(index), bn)
+
+      #Check the activation. 
+      #It is either Linear or a Leaky ReLU for YOLO
+      if activation == "leaky":
+        activn = nn.LeakyReLU(0.1, inplace = True)
+        module.add_module("leaky_{0}".format(index), activn)
+
+    #If it's an upsampling layer
+    #We use Bilinear2dUpsampling
+    elif (x["type"] == "upsample"):
+      stride = int(x["stride"])
+      upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
+      module.add_module("upsample_{}".format(index), upsample)
+
+
+
